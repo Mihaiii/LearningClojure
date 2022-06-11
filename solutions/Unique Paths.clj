0a36ac11fb15a6b1ae1d55560bd19a14f962c6ae
Mihai <mihai.chirculescu@yahoo.com>
2022-06-11 18:02:18 +0300

    Update Unique Paths solution.
    Use memoization.

--- solutions/Unique Paths.clj
+++ solutions/Unique Paths.clj
@@ -8,3 +8,13 @@
 (time (unique-paths 17 13))
 ;"Elapsed time: 1407.7039 msecs"
 ;30421755
+
+; taken from: https://github.com/m0wfo/clojure-dynamic/blob/4e5730eabb809fee95d81cce17e61dbfaf93869d/src/main/clojure/com/mowforth/dynamic/fib.clj
+; not yet clear how exactly it applyes memoization.
+(defn top-down [x y]
+  (with-redefs [unique-paths (memoize unique-paths)]
+    (unique-paths x y)))
+
+(time (top-down 17 13))
+;"Elapsed time: 0.6861 msecs"
+;30421755
